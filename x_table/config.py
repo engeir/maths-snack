5288e5cb2d6fdc395a43cc5418e05ac3b7827187
engeir <38856990+engeir@users.noreply.github.com>
2019-08-07 20:51:10 +0200

    Update config.py

--- x_table/config.py
+++ x_table/config.py
@@ -1,9 +1,19 @@
+import pygame as pg
+
 # CONSTANTS
-SCREEN_WIDTH = 900
-SCREEN_HEIGHT = SCREEN_WIDTH
+# SCREEN_WIDTH = 900
+# SCREEN_HEIGHT = SCREEN_WIDTH
+pg.display.init()
+a = pg.display.list_modes()
+SCREEN_WIDTH, SCREEN_HEIGHT = a[0][0], a[0][1]
+# SCREEN_WIDTH, SCREEN_HEIGHT = pg.display.Info().current_w, pg.display.Info().current_h
 FPS = 30
+
+# Color
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 PURPLE = (20, 2, 2)
+GREEN = (20, 170, 4)
+RED = (200, 10, 10)
 
-RADIUS = int(SCREEN_WIDTH / 2 - 60)
+RADIUS = 500  # int(SCREEN_WIDTH / 2 - 60)
