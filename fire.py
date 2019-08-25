cdc781ab2eda46ca02749045618ab4e568cfe259
engeir <38856990+engeir@users.noreply.github.com>
2019-08-25 20:25:58 +0200

    Create fire.py

---
+++ fire.py
@@ -0,0 +1,20 @@
+import matplotlib.pyplot as plt
+
+
+A229037_lst = []  # type: list
+total = 10**4
+
+for n in range(total):
+    i, j, b = 1, 1, set()
+    while n - 2 * i >= 0:
+        b.add(2 * A229037_lst[n - i] - A229037_lst[n - 2 * i])
+        i += 1
+        while j in b:
+            b.remove(j)
+            j += 1
+            print(f"{round(n / total * 100, 1)}%", end='\r')
+    A229037_lst.append(j)
+
+plt.figure()
+plt.plot(A229037_lst, 'o', markersize=1)
+plt.show()
