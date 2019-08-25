0fd853ad02a1f412f044c3e548a006103ab01102
engeir <38856990+engeir@users.noreply.github.com>
2019-08-25 20:40:15 +0200

    Update van_eck.py
    Added description to the method 'van_eck'.

--- van_eck.py
+++ van_eck.py
@@ -2,6 +2,19 @@ import matplotlib.pyplot as plt
 
 
 def van_eck(the_end):
+    """Make the van Eck-sequence.
+
+    The first entry is a(0) = 0. At a(n+1) you then ask weather you have seen a(n) before,
+    if no, a(n+1) = 0, if yes a(n+1) is equal to the amount of steps we must
+    take to get to the last time we saw a(n). For a(1), we have not seen a(0)=0 before, so a(1) = 0.
+    At a(2) we have seen a(1) = 0 before [a(0) is also 0], and it was one step backwards, so a(2) = 1.
+
+    Arguments:
+        the_end {int} -- how many entries we want to compute for the series
+
+    Returns:
+        list -- the series presented as a list
+    """
     c = 0
     series = [0]
     while c < the_end:
@@ -19,7 +32,3 @@ plt.plot(list(range(len(a))), a)
 plt.xlabel('Van Eck sequence index')
 plt.ylabel('Number')
 plt.show()
-
-
-for i in range(10):
-    print(i**2)
